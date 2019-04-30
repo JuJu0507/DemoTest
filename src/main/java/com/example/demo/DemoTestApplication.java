58c3c5e593b69717872645726821bae53922a0e1
zhulei <lei.zhu01@mljr.com>
2019-04-30 16:37:43 +0800

    test2

--- src/main/java/com/example/demo/DemoTestApplication.java
+++ src/main/java/com/example/demo/DemoTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoTestApplication {
     //test
+    //test2
     public static void main(String[] args) {
         SpringApplication.run(DemoTestApplication.class, args);
     }
